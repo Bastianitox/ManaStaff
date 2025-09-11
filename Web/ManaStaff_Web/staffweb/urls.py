7b30b379e6a8ef7a0ecc9badca3ebbd56fe34c0f
Bastianitox <bastiannunez54@gmail.com>
2025-09-11 15:23:54 -0300

    arreglo error de coma en urls

--- Web/ManaStaff_Web/staffweb/urls.py
+++ Web/ManaStaff_Web/staffweb/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
 
     # PAGINAS
     path('',index,name="index"),
-    path('recuperar_contrasena',recuperar_contrasena,name="recuperar_contrasena")
+    path('recuperar_contrasena',recuperar_contrasena,name="recuperar_contrasena"),
     path('inicio_solicitudes',inicio_solicitudes,name="inicio_solicitudes")
 
 ]
