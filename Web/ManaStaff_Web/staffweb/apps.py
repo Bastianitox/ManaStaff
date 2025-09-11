aa6f3bdd2e171a8f9436119a3defc2ba568e8c2f
Bastianitox <bastiannunez54@gmail.com>
2025-09-10 21:10:20 -0300

    esqueleto django

---
+++ Web/ManaStaff_Web/staffweb/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class StaffwebConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'staffweb'
---
+++ Web/ManaStaff_Web/staffweb/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+urlpatterns = [
+
+    # PAGINAS
+    path('',inicio,name="inicio"),
+
+]
