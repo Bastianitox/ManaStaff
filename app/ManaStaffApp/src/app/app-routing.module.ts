70d58def4b4ab00825f39c51141aca8a51810098
Bastianitox <bastiannunez54@gmail.com>
2025-10-27 18:58:16 -0300

    arreglado "ejemplo"

--- app/ManaStaffApp/src/app/app-routing.module.ts
+++ app/ManaStaffApp/src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'ejemplo',
+    loadChildren: () => import('./pages/ejemplo/ejemplo.module').then( m => m.EjemploPageModule)
   }
 ];
 @NgModule({
---
+++ app/ManaStaffApp/src/app/pages/ejemplo/ejemplo.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ejemplo',
+  templateUrl: './ejemplo.page.html',
+  styleUrls: ['./ejemplo.page.scss'],
+  standalone: false
+})
+export class EjemploPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
