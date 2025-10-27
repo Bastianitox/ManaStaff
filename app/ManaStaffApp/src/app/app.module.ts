6acbcdf5dcfec6e766a144f0fbd5dda4eca2e532
Bastianitox <bastiannunez54@gmail.com>
2025-10-27 15:23:02 -0300

    Configuracion inicial de ManaStaffApp

---
+++ app/ManaStaffApp/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { environment } from '../environments/environment';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+  ],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideStorage(() => getStorage()),
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ app/ManaStaffApp/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDrogTFQNg_BNb1qmkIhJ6cpppzPw-DLOo",
+    authDomain: "manastaff-7ef1d.firebaseapp.com",
+    databaseURL: "https://manastaff-7ef1d-default-rtdb.firebaseio.com",
+    projectId: "manastaff-7ef1d",
+    storageBucket: "manastaff-7ef1d.firebasestorage.app",
+    messagingSenderId: "409038016605",
+    appId: "1:409038016605:web:0ff9ded9533dcc28c1fdb4",
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
