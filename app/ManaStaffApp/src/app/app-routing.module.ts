ed804a116fa2fe2a3ffa4b8b6833698fe0a256f4
Bastianitox <bastiannunez54@gmail.com>
2025-10-30 19:35:59 -0300

    actualizacionn de CORS y eliminacion de TABS

--- app/ManaStaffApp/src/app/app-routing.module.ts
+++ app/ManaStaffApp/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   },
   {
     path: 'ejemplo',
@@ -61,10 +61,6 @@ const routes: Routes = [
   {
     path: 'recuperarcontra',
     loadChildren: () => import('./pages/recuperarcontra/recuperarcontra.module').then( m => m.RecuperarcontraPageModule)
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   }
 
 
